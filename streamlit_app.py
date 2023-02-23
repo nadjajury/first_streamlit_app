c524ba9a0ceb4c4478adeb92048df2df70b7eb8f
nadjajury <nadja.jury@gmail.com>
2023-02-23 13:18:49 +1300

    Update streamlit_app.py
    stopping

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display fruit response in table
 streamlit.dataframe(fruityvice_normalized)
  
+streamlit.stop() 
+ 
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
